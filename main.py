b918fcdc3f11d17ab7b4a490ee4f131e321b2c0a
Santiago <60336621+yakdudi12@users.noreply.github.com>
2023-09-27 11:08:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+Amino_dictionary = {"L": ["CUU", "CUC","CUA", "CUG","UUA","UUG"],
+                    "F": ["UUU","UUC"],"I":["AUU","AUC","AUA"],
+                    "M":"AUG","V":["GUU","GUC","GUA","GUG"],
+                    "S":["UCU","UCC","UCA","UCG","AGU","AGC"],
+                    "P":["CCU","CCC","CCA","CCG"],
+                    "T":["ACU","ACC","ACA","ACG"],
+                    "A":["GCU","GCC","GCA","GCG"],
+                    "Y":["UAU","UAC"],"H":["CAU","CAC"],
+                    "Q":["CAA","CAG"],"K":["AAA","AAG"],
+                    "E":["GAA","GAG"],"C":["UGU","UGC"],
+                    "W":"UGG","R":["CGU","CGC","CGA","CGG","AGA","AGG"],
+                    "G":["GGU","GGC","GGA","GGG"],"*":["UAA","UAG","UGA"],
+                    "N":["AAU","AAC"],"D":["GAU","GAC"]}
+print(Amino_dictionary)
